aeb840acfebb3651446bddfa3647f63f4337b021
Fjeanneret <florian.jeanneret@cea.fr>
2021-04-21 11:36:28 +0200

    Add golem

--- R/run_app.R
+++ R/run_app.R
@@ -1,13 +1,18 @@
 #' Run the Shiny Application
 #'
 #' @importFrom shiny shinyApp runApp
+#' @importFrom golem with_golem_options
 #' 
 #' @export
 #' @return Launches RShiny app
-run_app <- function() {
-    app = shinyApp(
-      ui = app_ui, 
-      server = app_server
-    )
-    runApp(app)
+run_app <- function(
+  ...
+) {
+  with_golem_options(
+      app = shinyApp(
+        ui = app_ui, 
+        server = app_server
+      ), 
+      golem_opts = list(...)
+  )
 }
